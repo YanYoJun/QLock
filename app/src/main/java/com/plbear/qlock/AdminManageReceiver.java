401eeffdc37a0b75d23696e4b0b2996550a6d120
yanyongjun <yanyojun@foxmail.com>
2017-01-16 22:26:39 +0800

    一键锁屏app开发完成

---
+++ app/src/main/java/com/plbear/qlock/AdminManageReceiver.java
@@ -0,0 +1,10 @@
+package com.plbear.qlock;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by Yanyongjun on 2017/1/16.
+ */
+
+public class AdminManageReceiver extends DeviceAdminReceiver {
+}
--- app/src/main/java/com/plbear/qlock/MainActivity.java
+++ app/src/main/java/com/plbear/qlock/MainActivity.java
@@ -1,13 +1,38 @@
 package com.plbear.qlock;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ComponentName mAdminName = null;
+    private static final String TAG = "plbear";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mAdminName = new ComponentName(this, AdminManageReceiver.class);
+
+        DevicePolicyManager mDPM = (DevicePolicyManager) this.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        if (!mDPM.isAdminActive(mAdminName)) {
+            showAdminManage();
+        }
+
+        if (mDPM.isAdminActive(mAdminName)) {
+            mDPM.lockNow();
+        }
+
+        finish();
+    }
+
+    private void showAdminManage() {
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,mAdminName);
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,R.string.app_name);
+        startActivityForResult(intent,-1);
     }
 }
